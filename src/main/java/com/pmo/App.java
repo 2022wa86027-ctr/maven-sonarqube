6170d036644034a6b1fb37b55087c9d897c688cd
2022wa86027-ctr <2022wa86027@wilp.bits-pilani.ac.in>
2026-09-05 00:49:12 +0530

    Create App.java

---
+++ src/main/java/com/pmo/App.java
@@ -0,0 +1,9 @@
+package com.pmo;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Maven SonarQube Demo");
+        System.out.println("Jenkins CI/CD Pipeline");
+    }
+}
